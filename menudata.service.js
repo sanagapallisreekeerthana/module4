eefdee640b46cbfa242b265d5475bf4282f382a1
sanagapallisreekeerthana <143833557+sanagapallisreekeerthana@users.noreply.github.com>
2024-09-26 14:21:14 +0530

    Create menudata.service.js

---
+++ menudata.service.js
@@ -0,0 +1,20 @@
+(function() {
+    angular.module('data')
+    .service('MenuDataService', ['$http', function($http) {
+        var service = this;
+
+        service.getAllCategories = function() {
+            return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/categories.json')
+                .then(function(response) {
+                    return response.data;
+                });
+        };
+
+        service.getItemsForCategory = function(categoryShortName) {
+            return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/' + categoryShortName + '.json')
+                .then(function(response) {
+                    return response.data;
+                });
+        };
+    }]);
+})();
